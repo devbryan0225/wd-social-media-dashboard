d43a31c05499cd8b5a25cb0611b18a6aa60ab5fc
devbryan0225 <devbryan0225@users.noreply.github.com>
2020-07-17 17:06:37 +0800

    Project completed.

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem'
+      },
+      letterSpacing: {
+        widestest: '.5em'
+      },
+      backgroundColor: {
+        'smd': 'var(--smd-main-background)',
+        'smd-card': 'var(--smd-card-background)',
+        'smd-card-hover': 'var(--smd-card-background-hover)'
+      },
+      textColor: {
+        'smd-primary': 'var(--smd-text-primary)',
+        'smd-secondary': 'var(--smd-text-secondary)',
+        'smd-overview': 'var(--smd-text-overview)'
+      },
+      flex: {
+        'fit-4': '0 1 23%'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
